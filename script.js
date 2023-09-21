bc9ab35dd3a62cd80343eb7f45e9fea9e4b5915f
chiknwy <chikogitasatria23@gmail.com>
2023-09-21 15:30:31 +0800

    push

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function calculateFibonacci() {
         const result = fibonacci(n);
         document.getElementById("fibonacci-result").textContent = result;
     } else {
-        alert("Masukkan angka positif.");
+        alert("Please enter a non-negative number.");
     }
 }
 
@@ -24,102 +24,60 @@ function fibonacci(n) {
 
     return fibCurrent;
 }
+
 function calculateVolume() {
     const selectedShape = document.getElementById("shape-select").value;
-    const kubusSide = parseFloat(document.getElementById("kubus-side").value);
-    const panjangBalok = parseFloat(document.getElementById("balok-panjang").value);
-    const lebarBalok = parseFloat(document.getElementById("balok-lebar").value);
-    const tinggiBalok = parseFloat(document.getElementById("balok-tinggi").value);
-    const bolaRadius = parseFloat(document.getElementById("bola-radius").value);
-    const tinggiTabung = parseFloat(document.getElementById("tabung-tinggi").value);
-    const tabungRadius = parseFloat(document.getElementById("tabung-radius").value);
-    const kerucutRadius = parseFloat(document.getElementById("kerucut-radius").value);
-    const kerucutTinggi = parseFloat(document.getElementById("kerucut-tinggi").value);
-    const limasAlas = parseFloat(document.getElementById("limas-luas").value);
-    const limasTinggi = parseFloat(document.getElementById("limas-tinggi").value);
+    const cubeSide = parseFloat(document.getElementById("cube-side").value);
+    const prismLength = parseFloat(document.getElementById("rectangular-prism-length").value);
+    const prismWidth = parseFloat(document.getElementById("rectangular-prism-width").value);
+    const prismHeight = parseFloat(document.getElementById("rectangular-prism-height").value);
+    const sphereRadius = parseFloat(document.getElementById("sphere-radius").value);
+    const cylinderRadius = parseFloat(document.getElementById("cylinder-radius").value);
+    const cylinderHeight = parseFloat(document.getElementById("cylinder-height").value);
+    const coneRadius = parseFloat(document.getElementById("cone-radius").value);
+    const coneHeight = parseFloat(document.getElementById("cone-height").value);
+    const pyramidBaseArea = parseFloat(document.getElementById("pyramid-base-area").value);
+    const pyramidHeight = parseFloat(document.getElementById("pyramid-height").value);
 
-    if (selectedShape === "kubus" && !isNaN(kubusSide) && kubusSide >= 0) {
-        const volume = kubusSide ** 3;
-        document.getElementById("volume-result").textContent = `Volume Kubus: ${volume.toFixed(2)}`;
+    if (selectedShape === "cube" && !isNaN(cubeSide) && cubeSide >= 0) {
+        const volume = cubeSide ** 3;
+        document.getElementById("volume-result").textContent = `Cube Volume: ${volume.toFixed(2)}`;
     } 
-    else if (selectedShape === "balok" && !isNaN(panjangBalok) && !isNaN(lebarBalok) && !isNaN(tinggiBalok) && panjangBalok >= 0 && lebarBalok >= 0 && tinggiBalok >= 0) {
-        const volume = panjangBalok * lebarBalok * tinggiBalok;
-        document.getElementById("volume-result").textContent = `Volume Balok: ${volume.toFixed(2)}`;
+    else if (selectedShape === "rectangular-prism" && !isNaN(prismLength) && !isNaN(prismWidth) && !isNaN(prismHeight) && prismLength >= 0 && prismWidth >= 0 && prismHeight >= 0) {
+        const volume = prismLength * prismWidth * prismHeight;
+        document.getElementById("volume-result").textContent = `Rectangular Prism Volume: ${volume.toFixed(2)}`;
     } 
-    else if (selectedShape === "bola" && !isNaN(bolaRadius) && bolaRadius >= 0) {
-        const volume = (4 / 3) * Math.PI * (bolaRadius ** 3);
-        document.getElementById("volume-result").textContent = `Volume Bola: ${volume.toFixed(2)}`;
+    else if (selectedShape === "sphere" && !isNaN(sphereRadius) && sphereRadius >= 0) {
+        const volume = (4 / 3) * Math.PI * (sphereRadius ** 3);
+        document.getElementById("volume-result").textContent = `Sphere Volume: ${volume.toFixed(2)}`;
     }
-    else if (selectedShape === "tabung" && !isNaN(tabungRadius) && !isNaN(tinggiTabung) && tabungRadius >= 0 && tinggiTabung >= 0) {
-        const volume = Math.PI * (tabungRadius ** 2) * tinggiTabung;
-        document.getElementById("volume-result").textContent = `Volume Tabung: ${volume.toFixed(2)}`;
+    else if (selectedShape === "cylinder" && !isNaN(cylinderRadius) && !isNaN(cylinderHeight) && cylinderRadius >= 0 && cylinderHeight >= 0) {
+        const volume = Math.PI * (cylinderRadius ** 2) * cylinderHeight;
+        document.getElementById("volume-result").textContent = `Cylinder Volume: ${volume.toFixed(2)}`;
     }
-    else if (selectedShape === "kerucut" && !isNaN(kerucutRadius) && !isNaN(kerucutTinggi) && kerucutRadius >= 0 && kerucutTinggi >= 0) {
-        const volume = (1 / 3) * Math.PI * (kerucutRadius ** 2) * kerucutTinggi;
-        document.getElementById("volume-result").textContent = `Volume Kerucut: ${volume.toFixed(2)}`;
+    else if (selectedShape === "cone" && !isNaN(coneRadius) && !isNaN(coneHeight) && coneRadius >= 0 && coneHeight >= 0) {
+        const volume = (1 / 3) * Math.PI * (coneRadius ** 2) * coneHeight;
+        document.getElementById("volume-result").textContent = `Cone Volume: ${volume.toFixed(2)}`;
     }
-    else if (selectedShape === "limas" && !isNaN(limasAlas) && !isNaN(limasTinggi) && limasAlas >= 0 && limasTinggi >= 0) {
-        const volume = (1 / 3) * limasAlas * limasTinggi;
-        document.getElementById("volume-result").textContent = `Volume Limas: ${volume.toFixed(2)}`;
+    else if (selectedShape === "pyramid" && !isNaN(pyramidBaseArea) && !isNaN(pyramidHeight) && pyramidBaseArea >= 0 && pyramidHeight >= 0) {
+        const volume = (1 / 3) * pyramidBaseArea * pyramidHeight;
+        document.getElementById("volume-result").textContent = `Pyramid Volume: ${volume.toFixed(2)}`;
     }
     else {
-        alert("Masukkan angka positif.");
+        alert("Please enter positive numbers.");
     }
 }
 
-
-
 document.getElementById("shape-select").addEventListener("change", function () {
     const selectedShape = this.value;
-    if (selectedShape === "kubus") {
-        document.getElementById("kubus-form").style.display = "block";
-        document.getElementById("balok-form").style.display = "none";
-        document.getElementById("bola-form").style.display = "none";
-        document.getElementById("tabung-form").style.display = "none";
-        document.getElementById("kerucut-form").style.display = "none";
-        document.getElementById("tabung-form").style.display = "none";
-        document.getElementById("limas-form").style.display = "none";
-    } 
-    else if (selectedShape === "balok") {
-        document.getElementById("kubus-form").style.display = "none";
-        document.getElementById("balok-form").style.display = "block";
-        document.getElementById("bola-form").style.display = "none";
-        document.getElementById("tabung-form").style.display = "none";
-        document.getElementById("kerucut-form").style.display = "none";
-        document.getElementById("limas-form").style.display = "none";
-    }
-    else if (selectedShape === "bola") {
-        document.getElementById("kubus-form").style.display = "none";
-        document.getElementById("balok-form").style.display = "none";
-        document.getElementById("bola-form").style.display = "block";
-        document.getElementById("tabung-form").style.display = "none";
-        document.getElementById("kerucut-form").style.display = "none";
-        document.getElementById("limas-form").style.display = "none";
-    }
-    else if (selectedShape === "tabung") {
-        document.getElementById("kubus-form").style.display = "none";
-        document.getElementById("balok-form").style.display = "none";
-        document.getElementById("bola-form").style.display = "none";
-        document.getElementById("kerucut-form").style.display = "none";
-        document.getElementById("tabung-form").style.display = "block";
-        document.getElementById("limas-form").style.display = "none";
-    }
-    else if (selectedShape === "kerucut") {
-        document.getElementById("kubus-form").style.display = "none";
-        document.getElementById("balok-form").style.display = "none";
-        document.getElementById("bola-form").style.display = "none";
-        document.getElementById("kerucut-form").style.display = "block";
-        document.getElementById("tabung-form").style.display = "none";
-        document.getElementById("limas-form").style.display = "none";
-    }
-    else if (selectedShape === "limas") {
-        document.getElementById("kubus-form").style.display = "none";
-        document.getElementById("balok-form").style.display = "none";
-        document.getElementById("bola-form").style.display = "none";
-        document.getElementById("kerucut-form").style.display = "none";
-        document.getElementById("tabung-form").style.display = "none";
-        document.getElementById("limas-form").style.display = "block";
-    }
+    const formIds = ["cube-form", "rectangular-prism-form", "sphere-form", "cylinder-form", "cone-form", "pyramid-form"];
 
-    
+    for (const formId of formIds) {
+        const form = document.getElementById(formId);
+        if (formId === `${selectedShape}-form`) {
+            form.style.display = "block";
+        } else {
+            form.style.display = "none";
+        }
+    }
 });

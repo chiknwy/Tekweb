6c87f737b7a3d72641478be49515c5266f159b54
chiknwy <chikogitasatria23@gmail.com>
2023-09-29 06:36:16 +0800

    push

--- porto/script.js
+++ porto/script.js
@@ -1,8 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     // Smooth scrolling for anchor links
     var scrollLinks = document.querySelectorAll('a[href^="#"]');
     for (var i = 0; i < scrollLinks.length; i++) {
-        scrollLinks[i].addEventListener('click', function(e) {
+        scrollLinks[i].addEventListener('click', function (e) {
             e.preventDefault();
             var targetId = this.getAttribute('href').substring(1);
             var targetElement = document.getElementById(targetId);
@@ -15,11 +15,29 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 
-    // Toggle the dropdown menu
+    // Show/hide back to top button
     const menuToggle = document.getElementById("menu-toggle");
     const menu = document.getElementById("menu");
 
     menuToggle.addEventListener("click", function () {
         menu.classList.toggle("hidden");
+        menuToggle.classList.toggle("rotate-90"); // Add Tailwind class to rotate the button
+
+        // Toggle between hamburger and X
+        if (menu.classList.contains("hidden")) {
+            // Change to hamburger icon
+            menuToggle.innerHTML = `
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
+                </svg>
+            `;
+        } else {
+            // Change to X icon
+            menuToggle.innerHTML = `
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+                </svg>
+            `;
+        }
     });
 });
